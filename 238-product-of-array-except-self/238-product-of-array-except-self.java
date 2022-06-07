c4c677f7304dcf66409fb33cea2720fd321b64e6
TariqRepos <104251502+TariqRepos@users.noreply.github.com>
2022-06-06 22:29:35 -0500

    Time: 2 ms (80.37%), Space: 57.9 MB (34.09%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,20 +1,17 @@
 class Solution {
-    // You could do division method, but if you
-    // cant you can do both pre and post 
-    // multiplcation
     public int[] productExceptSelf(int[] nums) {
-        int [] res = new int[nums.length];
-        Arrays.fill(res, 1);
-        int prefix = 1;
-        for(int i=0; i<res.length; i++){
-            res[i] *= prefix;
-            prefix *= nums[i];
+        int[] res = new int[nums.length];
+        
+        int val = 1;
+        for(int i=0; i<nums.length; i++){
+            res[i] = val;
+            val *= nums[i];
         }
         
-        int postfix = 1;
-        for(int i=res.length-1; i>-1; i--){
-            res[i] *= postfix;
-            postfix *= nums[i];
+        val = 1;
+        for(int i=nums.length-1; i>-1; i--){
+            res[i] *= val;
+            val *= nums[i];
         }
         
         return res;
