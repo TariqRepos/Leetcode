3cb07aef5ba9c6c92a8fff14f442320c00ebad57
TariqRepos <104251502+TariqRepos@users.noreply.github.com>
2022-06-25 17:54:04 -0500

    Time: 283 ms (5.01%), Space: 54.5 MB (5.04%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -11,20 +11,17 @@ class Solution {
         for(int i = 0; i <= n; i++){
             System.out.println();
             System.out.println("i: " + i);
-            while (!stack.isEmpty() && (i==n || (min && nums[stack.peek()]>nums[i]) || (!min && nums[stack.peek()]<nums[i]))){
-                int pop_i = stack.pop();
-                int value = nums[pop_i];
-                int prev_i = stack.isEmpty() ? -1 : stack.peek();
-                int left = pop_i-prev_i;
-                long dist = (i-pop_i)*(pop_i-prev_i);
-                sum += dist*value;
-                System.out.println("stack pop_i : " + pop_i);
-                System.out.println("pop_i value : " + value);
-                System.out.println("(i - pop_i) : " + (i-pop_i));
-                // System.out.println("pop_i-peek(): " + (pop_i-stack.peek()));
-                System.out.println("distance    : " + dist);
-                System.out.println("sum         : " + sum);
+            while (!stack.isEmpty() && (i==n || 
+                    (min && nums[stack.peek()] > nums[i]) ||  // MIN sum
+                    (!min && nums[stack.peek()] < nums[i]))){ // MAX sum
+
+                int pop_i = stack.pop(); // Current index in array
+                int prev_i = stack.isEmpty() ? -1 : stack.peek(); // Prev min or max
                 
+                int left = pop_i - prev_i; // Left boundry
+                int right = i - pop_i;     // Right boundry
+                
+                sum += left * right * (long)nums[pop_i];
             }
             stack.push(i);
         }
