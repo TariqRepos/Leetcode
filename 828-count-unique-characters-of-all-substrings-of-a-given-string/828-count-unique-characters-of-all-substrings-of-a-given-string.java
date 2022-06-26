66f9990019a54dde74fbca96b67e0a9bef1de2b2
TariqRepos <104251502+TariqRepos@users.noreply.github.com>
2022-06-26 18:29:11 -0500

    Time: 25 ms (55.11%), Space: 62.1 MB (26.79%) - LeetHub

--- 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
+++ 828-count-unique-characters-of-all-substrings-of-a-given-string/828-count-unique-characters-of-all-substrings-of-a-given-string.java
@@ -1,19 +1,28 @@
 class Solution {
     public int uniqueLetterString(String S) {
-        int[][] index = new int[26][2];
+        // Records last two occurrence index for every upper characters
+        int[][] index = new int[26][2]; 
+        
+        // Initialise all values in index to -1
         for (int i = 0; i < 26; ++i) Arrays.fill(index[i], -1);
         
         int res = 0;
         int N = S.length();
-        int mod = (int)Math.pow(10, 9) + 7;
         
+        // Loop on string S, for every character c, update its last two occurrence index to index[c]
         for (int i = 0; i < N; ++i) {
             int c = S.charAt(i) - 'A';
-            res = res + (i - index[c][1]) * (index[c][1] - index[c][0]);
+            
+            // Length btw curr to prev ocurrance * length btw prev to prev prev ocurrance
+            res += (i - index[c][1]) * (index[c][1] - index[c][0]);
             index[c] = new int[] {index[c][1], i};
         }
+        
+        // Loop through Char array and get result
         for (int c = 0; c < 26; ++c)
-            res = res + (N - index[c][1]) * (index[c][1] - index[c][0]);
+            // Length btw end to prev ocurrance * length btw prev to prev prev ocurrance
+            res += (N - index[c][1]) * (index[c][1] - index[c][0]);
+        
         return res;
     }
 }
