a19b61f05ccdab4203ca90e8ef86fe3dd5035cdb
TariqRepos <104251502+TariqRepos@users.noreply.github.com>
2022-07-11 14:30:54 -0500

    Time: 15 ms (49.26%), Space: 49.9 MB (74.63%) - LeetHub

--- 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
+++ 926-flip-string-to-monotone-increasing/926-flip-string-to-monotone-increasing.java
@@ -1,4 +1,8 @@
 class Solution {
+// one is the counter if you were to flip 1s up to current index
+// flip is the counter for the minimum flips you could do up to current index
+
+// so during the looping, you are considering which requires less flips.
     public int minFlipsMonoIncr(String s) {
         int one = 0;
         int flip =0;
